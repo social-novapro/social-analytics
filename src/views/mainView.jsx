7b151f8d600a232cc710cc3c5f04234e2199c52e
Daniel Kravec <daniel@kravec.com>
2022-03-28 01:44:56 -0400

    1.0 (5.2022.03.28)

--- src/views/mainView.jsx
+++ src/views/mainView.jsx
@@ -7,15 +7,29 @@ export default function MainView({runtime}) {
             <div className='App'>
                 <h1>Interact Analytics</h1>
             </div>
-              
             <div>
-                <p>current runtime: {runtime}</p>
-                <p>This site is to check out all the analytics about Interact</p>
-                <p className='padding20'>Currently there are 4 different graphs:</p>
-                <Link className='buttonStyled buttonsDesign' to="/1">Connections / User</Link>
-                <Link className='buttonStyled buttonsDesign' to="/2">Connections / Day</Link>
-                <Link className='buttonStyled buttonsDesign' to="/3">Connections / User / Day</Link>
-                <Link className='buttonStyled buttonsDesign' to="/4">First Connection of User</Link>
+                <div className='padding20'>
+                    <p>current runtime: {runtime}</p>
+                    <hr></hr>
+                    <p>This site is to check out all the analytics about Interact</p>
+                    <p>Currently there are 4 different graphs:</p>
+                </div>
+                <div>
+                    <Link className='buttonStyled buttonsDesign' to="/1">Connections / User</Link>
+                    <p className='padding20'>This graph shows the number of connections per user</p>
+                </div>
+                <div>
+                    <Link className='buttonStyled buttonsDesign' to="/2">Connections / Day</Link>
+                    <p className='padding20'>This graph shows the number of connections each day</p>
+                </div>
+                <div>
+                    <Link className='buttonStyled buttonsDesign' to="/3">Connections / User / Day</Link>
+                    <p className='padding20'>This graph shows the number of connections per user each day</p>
+                </div>
+                <div>
+                    <Link className='buttonStyled buttonsDesign' to="/4">First Connection of User</Link>
+                    <p className='padding20'>This graph shows the first connection of each user</p>
+                </div>
             </div>
         </div>
     )
